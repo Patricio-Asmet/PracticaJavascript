0833ba5b46217bed0e442e26b2bd50fd21ec9f89
Patricio Asmet <patricioasmet@gmail.com>
2023-10-12 22:44:45 -0300

    agregamos comentarios y una variable

---
+++ js/1-variables.js
@@ -0,0 +1,11 @@
+// Comentario de una sola linea 
+
+/*
+ Comentario 
+ De multiples
+ Lineas
+*/
+
+//declarar una variable (let[cambia valor] - const[no cambia valor] , "var" ya no se utiliza)
+
+let anioActual = 2023; //texto entre comillas
