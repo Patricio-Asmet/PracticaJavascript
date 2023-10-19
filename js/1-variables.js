0538399bb50bf330f98c641a08b1f0c4e044a7b9
Patricio Asmet <patricioasmet@gmail.com>
2023-10-19 18:02:30 -0300

    se agregaron variables y se los mostró en consola y ventana como msj emergente

--- js/1-variables.js
+++ js/1-variables.js
@@ -5,7 +5,33 @@
  De multiples
  Lineas
 */
+//ECMA nombre técnico
 
 //declarar una variable (let[cambia valor] - const[no cambia valor] , "var" ya no se utiliza)
 
-let anioActual = 2023; //texto entre comillas
+let anioActual = 2023; 
+const url ="https://rollingcodeschool.com/"  //texto entre comillas
+
+//Console log:
+console.log("Este es un mensaje en consola");
+console.log(anioActual);
+console.log(url);
+console.log("Año: " + anioActual) //Agregar una variable a un texto, concatenando con el signo +
+console.log("Año: " + (anioActual + 2)) //Operacion artimética con una variable y concatenar con un texto
+
+//Cambiar el valor de una variable
+anioActual = 2024;
+console.log(anioActual);
+
+// url='otro valor';
+console.log(url);
+
+document.write('<h1>Prueba de <br>titulo</h1>');
+document.write('Año: '+anioActual + '<br>');
+document.write('Año: '+anioActual);
+
+// ventanas emergentes
+alert('Esto es una prueba de alerta');
+
+let color = prompt('Ingrese un color');
+console.log(color);
