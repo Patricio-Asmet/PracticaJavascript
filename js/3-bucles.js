bd31e92d92c789d794635113983a850a69ca3625
Patricio Asmet <patricioasmet@gmail.com>
2023-10-24 18:24:22 -0300

    vimos ejemplos de bucles

--- js/3-bucles.js
+++ js/3-bucles.js
@@ -7,13 +7,28 @@
     }
 */
 
-let renglon = 1;
+/* let renglon = 1;
 
 while(renglon <= 10){
-    document.write(`<p>Renglon numero 1</p>`);
-    renglon = renglon + 1;
+    document.write(`<p>Renglon numero ${renglon}</p>`);
+    renglon ++; // ó = renglon + 1;
 }
-
+*/
 
 //do while
+
+let renglon = 1;
+
+do{
+    document.write(`<p>Renglon numero ${renglon}</p>`);
+    renglon ++;
+}while(renglon <= 10)
+
+
 //for
+
+// for(inicializar una variable; condicion logica; incrementar o decrementar una variable)
+
+for(let contador = 5; contador >= 1; contador --){
+    document.write(`<p>Cuenta regresiva ${contador}</p>`);
+}
