d8674ab581fd5edbac9bcff1f829ae9772870ee9
Patricio Asmet <patricioasmet@gmail.com>
2023-10-24 18:02:08 -0300

    se agrega ejercicio 7 incompleto

---
+++ js/3-bucles.js
@@ -0,0 +1,14 @@
+// while
+
+/*
+    while(condicion logica){
+        aqui va el codigo que quiero repetir varias veces
+        agregar un codigo para cambiar la condicion logica
+    }
+*/
+
+let renglon = 1;
+document.write(<p></p>)
+
+//do while
+//for
