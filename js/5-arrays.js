5b68e1ed10a991f8d9d50d72b3f551e1757165cb
Patricio Asmet <patricioasmet@gmail.com>
2023-11-02 17:20:55 -0300

    agregamos metodos para agregar, modificar o borrar elementos de un array

--- js/5-arrays.js
+++ js/5-arrays.js
@@ -21,3 +21,72 @@ for(let i = 0; i < peliculas.length; i++){
     document.write(`<li>${peliculas[i]}</li>`)
 }
 document.write(`</ul>`)
+
+// AGREGAR ELEMENTOS AL PRINCIPIO DEL ARRAY
+
+peliculas.unshift('el grinch', 'mi pobre angelito');
+document.write(`<h2>Lista de peliculas + 2 elementos (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
+
+peliculas.splice(5,0, 'martes 13 🔪');
+document.write(`<h2>Lista de peliculas + 2 elementos (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
+
+// AGREGAR ELEMENTO AL FINAL DEL ARRAY
+
+peliculas.push('gladiador');
+document.write(`<h2>Lista de peliculas + 2 elementos (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
+
+// MODIFICAR ELEMENTOS DEL ARRAY
+
+peliculas[7] = 'el padrino 1';
+
+document.write(`<h2>Lista de peliculas + 2 elementos (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
+
+// QUITAR ELEMENTO DE LA POSICION 0
+
+peliculas.shift();
+document.write(`<h2>Lista de peliculas - un elemento (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
+
+// borrar elemetos del array
+
+peliculas.splice(7,1); // el primer valor para la posicion y el segundo cuantos se borran a partir de la posicion 
+document.write(`<h2>Lista de peliculas - un elemento (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
+
+// quitar el ultimo elemento
+
+peliculas.pop();
+document.write(`<h2>Lista de peliculas - un elemento (${peliculas.length}) </h2>`)
+document.write(`<ul>`)
+for(let i = 0; i < peliculas.length; i++){
+    document.write(`<li>${peliculas[i]}</li>`)
+}
+document.write(`</ul>`) 
